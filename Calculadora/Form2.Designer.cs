2559fffdb4bc3e9fda50173b5ca0df2bc1b2106c
iamkai98 <kapconc2009@gmail.com>
2025-03-12 16:53:42 -0300

    terminar

--- Calculadora/Form2.Designer.cs
+++ Calculadora/Form2.Designer.cs
@@ -29,82 +29,133 @@
         private void InitializeComponent()
         {
             label1 = new Label();
-            textBox1 = new TextBox();
-            textBox2 = new TextBox();
-            textBox3 = new TextBox();
+            textBoxCM = new TextBox();
+            textBoxKG = new TextBox();
             label2 = new Label();
             label3 = new Label();
-            label4 = new Label();
+            labelResultado = new Label();
+            buttonCalcular = new Button();
+            label5 = new Label();
+            label6 = new Label();
+            radioButtonFeminino = new RadioButton();
+            radioButtonMasculino = new RadioButton();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(40, 52);
+            label1.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(110, 62);
             label1.Name = "label1";
-            label1.Size = new Size(173, 15);
+            label1.Size = new Size(137, 30);
             label1.TabIndex = 0;
-            label1.Text = "Calcular perímetro do triângulo";
+            label1.Text = "Calcular IMC";
             // 
-            // textBox1
+            // textBoxCM
             // 
-            textBox1.Location = new Point(153, 123);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(100, 23);
-            textBox1.TabIndex = 1;
+            textBoxCM.Location = new Point(158, 184);
+            textBoxCM.Name = "textBoxCM";
+            textBoxCM.Size = new Size(100, 23);
+            textBoxCM.TabIndex = 1;
             // 
-            // textBox2
+            // textBoxKG
             // 
-            textBox2.Location = new Point(153, 176);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(100, 23);
-            textBox2.TabIndex = 2;
-            // 
-            // textBox3
-            // 
-            textBox3.Location = new Point(153, 228);
-            textBox3.Name = "textBox3";
-            textBox3.Size = new Size(100, 23);
-            textBox3.TabIndex = 3;
+            textBoxKG.Location = new Point(158, 237);
+            textBoxKG.Name = "textBoxKG";
+            textBoxKG.Size = new Size(100, 23);
+            textBoxKG.TabIndex = 2;
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(76, 123);
+            label2.Location = new Point(57, 187);
             label2.Name = "label2";
-            label2.Size = new Size(50, 15);
+            label2.Size = new Size(95, 15);
             label2.TabIndex = 4;
-            label2.Text = "Lado A: ";
+            label2.Text = "Digite sua altura:";
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(76, 176);
+            label3.Location = new Point(62, 240);
             label3.Name = "label3";
-            label3.Size = new Size(46, 15);
+            label3.Size = new Size(90, 15);
             label3.TabIndex = 5;
-            label3.Text = "Lado B:";
+            label3.Text = "Digite seu peso:";
             // 
-            // label4
+            // labelResultado
             // 
-            label4.AutoSize = true;
-            label4.Location = new Point(76, 233);
-            label4.Name = "label4";
-            label4.Size = new Size(38, 15);
-            label4.TabIndex = 6;
-            label4.Text = "label4";
+            labelResultado.AutoSize = true;
+            labelResultado.Location = new Point(185, 348);
+            labelResultado.Name = "labelResultado";
+            labelResultado.Size = new Size(0, 15);
+            labelResultado.TabIndex = 6;
+            // 
+            // buttonCalcular
+            // 
+            buttonCalcular.Location = new Point(172, 294);
+            buttonCalcular.Name = "buttonCalcular";
+            buttonCalcular.Size = new Size(75, 23);
+            buttonCalcular.TabIndex = 7;
+            buttonCalcular.Text = "Calcular";
+            buttonCalcular.UseVisualStyleBackColor = true;
+            buttonCalcular.Click += buttonCalcular_Click;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(267, 187);
+            label5.Name = "label5";
+            label5.Size = new Size(24, 15);
+            label5.TabIndex = 8;
+            label5.Text = "cm";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(267, 240);
+            label6.Name = "label6";
+            label6.Size = new Size(20, 15);
+            label6.TabIndex = 9;
+            label6.Text = "kg";
+            // 
+            // radioButtonFeminino
+            // 
+            radioButtonFeminino.AutoSize = true;
+            radioButtonFeminino.Location = new Point(100, 127);
+            radioButtonFeminino.Name = "radioButtonFeminino";
+            radioButtonFeminino.Size = new Size(75, 19);
+            radioButtonFeminino.TabIndex = 10;
+            radioButtonFeminino.TabStop = true;
+            radioButtonFeminino.Text = "Feminino";
+            radioButtonFeminino.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonMasculino
+            // 
+            radioButtonMasculino.AutoSize = true;
+            radioButtonMasculino.Location = new Point(185, 127);
+            radioButtonMasculino.Name = "radioButtonMasculino";
+            radioButtonMasculino.Size = new Size(80, 19);
+            radioButtonMasculino.TabIndex = 11;
+            radioButtonMasculino.TabStop = true;
+            radioButtonMasculino.Text = "Masculino";
+            radioButtonMasculino.UseVisualStyleBackColor = true;
             // 
             // NewForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Controls.Add(label4);
+            Controls.Add(radioButtonMasculino);
+            Controls.Add(radioButtonFeminino);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(buttonCalcular);
+            Controls.Add(labelResultado);
             Controls.Add(label3);
             Controls.Add(label2);
-            Controls.Add(textBox3);
-            Controls.Add(textBox2);
-            Controls.Add(textBox1);
+            Controls.Add(textBoxKG);
+            Controls.Add(textBoxCM);
             Controls.Add(label1);
             Name = "NewForm";
             Text = "NewForm";
@@ -115,11 +166,15 @@
         #endregion
 
         private Label label1;
-        private TextBox textBox1;
-        private TextBox textBox2;
-        private TextBox textBox3;
+        private TextBox textBoxCM;
+        private TextBox textBoxKG;
         private Label label2;
         private Label label3;
-        private Label label4;
+        private Label labelResultado;
+        private Button buttonCalcular;
+        private Label label5;
+        private Label label6;
+        private RadioButton radioButtonFeminino;
+        private RadioButton radioButtonMasculino;
     }
 }
--- Calculadora/Form2.cs
+++ Calculadora/Form2.cs
@@ -16,5 +16,76 @@ namespace Calculadora
         {
             InitializeComponent();
         }
+
+        private bool ValidarForm()
+        {
+            string centimetros = textBoxCM.Text;
+            string kilogramas = textBoxKG.Text;
+
+            if (string.IsNullOrWhiteSpace(centimetros) || !textBoxCM.Text.All(char.IsNumber))
+            {
+                MessageBox.Show("Por favor, preencha os campos corretamente e digite apenas números em centímetros!");
+                return false;
+            }
+
+            if (string.IsNullOrWhiteSpace(kilogramas) || !textBoxKG.Text.All(char.IsNumber))
+            {
+                MessageBox.Show("Por favor, preencha os campos corretamente e digite apenas números em kilogramas!");
+                return false;
+            }
+
+            return true;
+        }
+               
+        
+
+        private void buttonCalcular_Click(object sender, EventArgs e)
+        {
+            if (!ValidarForm()) 
+            {
+                return;
+            }
+
+            string centimetros = textBoxCM.Text;
+            string kilogramas = textBoxKG.Text;
+
+            float alturaMetros = float.Parse(centimetros) / 100;//CONVERTENDO CENTÍMETROS PARA METROS
+            float resultado = float.Parse(kilogramas) / (alturaMetros) * (alturaMetros); //FÓRMULA DO IMC
+
+
+
+            if (radioButtonFeminino.Checked) //Button Feminino 
+            {
+                    if (resultado < 19.1)
+                    {
+                        labelResultado.Text = $"Seu IMC é: {resultado:F2}! Você está abaixo do peso";
+                    }
+
+                    else if (resultado < 25.8)
+                    {
+                        labelResultado.Text = $"Seu IMC é: {resultado:F2}! Você está com o peso normal";
+                    }
+                    else if (resultado < 27.3)
+                    {
+                        labelResultado.Text = $"Seu IMC é: {resultado:F2}! Você está um pouco acima do peso";
+                    }
+                    else if (resultado < 32.3)
+                    {
+                        labelResultado.Text = $"Seu IMC é: {resultado:F2}! Você está acima do peso";
+                    }
+                    else
+                    {
+                        labelResultado.Text = $"Seu IMC é: {resultado:F2}! Você está obeso";
+                    }
+            }
+            {
+
+
+            }
+
+
+
+
+        }
     }
 }
